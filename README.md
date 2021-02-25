00977b92e222325189c78515bd98863479114e43
Yongveloper <aaa9015@naver.com>
2021-02-25 21:48:31 +0900

    Modify list of README.md, Add index.js to all folders

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # YOOKS
+
+Collection of React Hooks
+
+- [x] useInput
+- [x] useTabs
+- [x] useTitle
+- [x] useClick
+- [x] useHover
+- [x] useConfirm
+- [x] usePreventLeave
+- [ ] useFadIn
+- [ ] useFullscreen
+- [ ] useNetwork
+- [ ] useNotification
+- [ ] useScroll
+- [ ] useAxios
---
+++ useHover/useHover.js
@@ -0,0 +1,15 @@
+export const useClick = (onHover) => {
+  if (typeof onHover !== 'function') return;
+  const element = useRef();
+  useEffect(() => {
+    if (element.current) {
+      element.current.addEventListener('mouseenter', onHover);
+    }
+    return () => {
+      if (element.current) {
+        element.current.removeEventListener('mouseenter', onHover);
+      }
+    };
+  }, []);
+  return element;
+};
